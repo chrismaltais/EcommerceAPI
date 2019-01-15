9fcfc96c1183042a082602c25c717784fad6be8e
Chris Maltais <chris.maltais@hotmail.ca>
2019-01-15 18:16:12 -0500

    ✨ Add PATCH /products/:sku to purchase products
     #3

--- server/controllers/product.controller.js
+++ server/controllers/product.controller.js
@@ -21,15 +21,32 @@ async function getStockedProducts() {
 
 async function getProductBySKU(sku) {
     try {
-        let product = await Product.find({ sku: sku })
+        let product = await Product.findOne({ sku: sku })
         return product;
     } catch (e) {
         return {error: e};
     }
 };
 
+async function purchaseProductBySKU(sku) {
+    try {
+        let productExist = await Product.findOne({"sku": sku });
+        if (!productExist) {
+            return null;
+        }
+        if (productExist.inventory_count <= 0) {
+            return 0;
+        }
+        let product = await Product.findOneAndUpdate({"sku": sku }, {$inc : {"inventory_count" : -1} }, {new: true});
+        return product;
+    } catch (e) {
+        return {error: e};
+    }
+}
+
 module.exports = {
     getAllProducts,
     getStockedProducts,
-    getProductBySKU
+    getProductBySKU,
+    purchaseProductBySKU
 }
--- server/test/product.test.js
+++ server/test/product.test.js
@@ -6,12 +6,12 @@ const {testProducts} = require('./seed/seed');
 
 describe('GET /products', () => {
     it('should get all products', async () => {
-        let response = await request(app).get('/products').expect(200)
+        let response = await request(app).get('/products').expect(200);
         expect(response.body.length).toBe(3);
     });
 
     it('should get only stocked products if ?stocked=true', async () => {
-        let response = await request(app).get('/products?stocked=true').expect(200)
+        let response = await request(app).get('/products?stocked=true').expect(200);
         expect(response.body.length).toBe(2);
 
         for (i = 0; i < response.body.length; i++) {
@@ -20,7 +20,7 @@ describe('GET /products', () => {
     });
 
     it('should get all products if ?stocked=false', async () => {
-        let response = await request(app).get('/products?stocked=false').expect(200)
+        let response = await request(app).get('/products?stocked=false').expect(200);
         expect(response.body.length).toBe(3);
     });
 });
@@ -28,19 +28,45 @@ describe('GET /products', () => {
 describe('GET /products/:sku', () => {
     it('should return product document', async () => {
         let testSKU = testProducts[0].sku;
-        let response = await request(app).get(`/products/${testSKU}`).expect(200)
-        expect(response.body.length).toBe(1);
-
-        expect(response.body[0].sku).toBe(testSKU);
-    })
+        let response = await request(app).get(`/products/${testSKU}`).expect(200);
+        expect(response.body).toMatchObject(testProducts[0]);
+    });
 
     it('should return 404 if product not found', async () => {
-        let fakeSKU = 5
-        let response = await request(app).get(`/products/${fakeSKU}`).expect(404)
-    })
+        let fakeSKU = 5;
+        let response = await request(app).get(`/products/${fakeSKU}`).expect(404);
+    });
 
     it('should return 400 if SKU is NaN', async () => {
-        let fakeSKU = 'isThisASKU'
-        let response = await request(app).get(`/products/${fakeSKU}`).expect(400)
-    })
+        let fakeSKU = 'isThisASKU';
+        let response = await request(app).get(`/products/${fakeSKU}`).expect(400);
+    });
+});
+
+describe('PATCH /products/:sku', () => {
+    it('should decrement a product inventory count by 1', async () => {
+        let testSKU = testProducts[0].sku;
+        let response = await request(app).patch(`/products/${testSKU}`).expect(200);
+
+        expect(response.body.sku).toBe(testSKU);
+        expect(response.body.inventory_count).toBe(testProducts[0].inventory_count - 1);
+
+        let productFromDB = await Product.findOne({"sku" : testSKU});
+        expect(productFromDB.inventory_count).toBe(testProducts[0].inventory_count - 1);
+    });
+
+    it('should return 404 if product not found', async () => {
+        let fakeSKU = 5;
+        let response = await request(app).patch(`/products/${fakeSKU}`).expect(404);
+    });
+
+    it('should return 400 if SKU is NaN', async () => {
+        let fakeSKU = 'isThisASKU';
+        let response = await request(app).patch(`/products/${fakeSKU}`).expect(400);
+    });
+
+    it('should return 404 if product has no inventory', async () => {
+        let testSKU = testProducts[2].sku;
+        let response = await request(app).patch(`/products/${testSKU}`).expect(404);
+    });
 })
